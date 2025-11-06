0eb1ac7c8174fdaa7aee3fc443260f2c29df0058
ezramchen <ezra.chen@sjsu.edu>
2025-11-05 18:19:36 -0800

    sop file

--- Core/Src/sop.c
+++ Core/Src/sop.c
@@ -11,14 +11,16 @@ float gt(uint16_t cell_temp_lowest) {
     if (45 < cell_temp_lowest && cell_temp_lowest < 60) return (60 - cell_temp_lowest) / 15; // hot full 45
     return 0;
 }
+//add cold stop/full hot full in header
 
 // soc currently not scaled properly for these calculations
+// needs nominal_capacity to reduce to fraction form
 float gz(uint32_t soc) {
-    if (0.02 < soc && soc <= 0.05) return (soc - 0.02)/0.03; // low ramp 0.02
-    if (0.05 < soc && soc <= 0.9) return 1; // low full 0.05
-    if (0.9 < soc && soc <= 0.97) return (0.97 - soc)/0.07; // high ramp 0.9
+    if (20 < soc && soc <= 50) return 1000*(soc - 20)/30; // low ramp 0.02
+    if (50 < soc && soc <= 900) return 1000; // low full 0.05
+    if (900 < soc && soc <= 970) return 1000*(970 - soc)/70; // high ramp 0.9
     return 0;
-}
+} //currently returns millifraction
 
 // need proper units/scaling principles for SoP calculation
 uint32_t SoPcalc(uint32_t soc, uint16_t cell_temp_lowest, float R0, float Inom) {
